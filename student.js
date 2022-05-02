cfd7bf307efa2f0a84ad382da2256f3edfe69cc7
Leslie <103486449+LeslieFritas95@users.noreply.github.com>
2022-05-02 13:25:34 +0200

    agg

---
+++ student.js
@@ -0,0 +1,57 @@
+class Student{
+
+    constructor(name, sourname, yob, gender, gradeArray){
+       this.name = name; 
+       this.sourname = sourname;
+       this.yob = yob;
+       this.gender = gender;
+       this.gradeArray = gradeArray;
+    }
+    calculateGradeMean(){
+        let sum = 0
+        for (let i = 0; i < this.gradeArray.length; i++) {
+            const grade = this.gradeArray[i];
+            sum = sum + grade;
+            
+        }
+        const mean = sum / this.gradeArray.length;
+
+        return mean;
+    }
+
+    calculateAge(){
+        const now = new Date();
+        const year = now.getFullYear();
+        const age = year - this.yob;
+        return age;   
+    }
+
+    toString(){
+        
+        const genere =  this.gender === 'f' ? 'femmina' : 'maschio';
+
+        const studentString = 'Nome: ' + this.name + '\n' + 
+                              'Cognome: ' + this.surname + '\n' +
+                              'Età: ' + this.calculateAge() + '\n'+
+                              'Genere: ' + genere + '\n' +
+                              'Media: ' + this.calculateGradeMean();
+        return studentString
+    }
+
+ }
+
+ //class athlete:
+
+//  properties:
+//  -name
+//  -surname
+//  -yob
+//  -Sport
+//  -palmares
+//  -genere
+//  -isretired
+
+//  functions:
+//  -getAge()
+//  -bestResult()
+//  -toString()
